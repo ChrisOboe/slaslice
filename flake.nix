1798abdb0554e5a7edf69d07ba2f87537c6239c3
ChrisOboe <chris@oboe.email>
2024-04-01 19:52:02 +0200

    initial working version

--- flake.nix
+++ flake.nix
@@ -7,12 +7,144 @@
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
   in rec {
     packages.x86_64-linux = {
+      open3d = pkgs.llvmPackages.libcxxStdenv.mkDerivation rec {
+        pname = "open3d";
+        version = "0.18.0";
+
+        src = pkgs.fetchFromGitHub {
+          owner = "isl-org";
+          repo = "Open3D";
+          rev = "v${version}";
+          hash = "sha256-VMykWYfWUzhG+Db1I/9D1GTKd3OzmSXvwzXwaZnu8uI=";
+        };
+
+        nativeBuildInputs = with pkgs; [cmake ninja];
+        buildInputs = with pkgs; [
+          curl.dev
+          openssl
+          assimp
+          eigen
+          embree
+          fmt
+          glew
+          glfw
+          imgui
+          libjpeg
+          jsoncpp
+          lzfse
+          msgpack
+          nanoflann
+          openssl
+          libpng
+          qhull
+          tbb
+          tinyobjloader
+          vtk
+          zeromq
+          llvmPackages.libcxx
+          llvmPackages.openmp
+          msgpack-cxx
+          boost
+        ];
+        cmakeFlags = [
+          "-DBUILD_SHARED_LIBS=ON"
+          # disable unneeded stuff
+          "-DBUILD_EXAMPLES=OFF"
+          "-DBUILD_PYTHON_MODULE=OFF"
+          "-DBUILD_JUPYTER_EXTENSION=OFF"
+          "-DBUILD_CUDA_MODULE=OFF"
+          "-DBUILD_ISPC_MODULE=OFF"
+          "-DENABLE_HEADLESS_RENDERING=OFF"
+          # enable stuff
+          "-DBUILD_GUI=OFF" # wants to download mesa :(
+          "-DWITH_OPENMP=ON"
+          "-DWITH_IPPICV=ON"
+          "-DBUILD_FILAMENT_FROM_SOURCE=ON"
+          # use system stuff
+          "-DUSE_SYSTEM_ASSIMP=ON"
+          "-DUSE_SYSTEM_CURL=OFF" # problems with openssl
+          "-DUSE_SYSTEM_CUTLASS=OFF" # not packaged
+          "-DUSE_SYSTEM_EIGEN3=ON"
+          "-DUSE_SYSTEM_EMBREE=ON"
+          "-DUSE_SYSTEM_FILAMENT=OFF" # not packaged
+          "-DUSE_SYSTEM_FMT=ON"
+          "-DUSE_SYSTEM_GLEW=ON"
+          "-DUSE_SYSTEM_GLFW=ON"
+          #"-DUSE_SYSTEM_GOOGLETEST=ON"
+          "-DUSE_SYSTEM_IMGUI=ON"
+          "-DImGui_LIBRARY=${pkgs.imgui}/lib/libimgui"
+          "-DCPP_LIBRARY=${pkgs.llvmPackages.libcxx}/lib/libc++"
+          "-DCPPABI_LIBRARY=${pkgs.llvmPackages.libcxx}/lib/libc++abi"
+          "-DUSE_SYSTEM_JPEG=ON"
+          "-DUSE_SYSTEM_JSONCPP=ON"
+          "-DUSE_SYSTEM_LIBLZF=OFF" # not packaged
+          "-DUSE_SYSTEM_MSGPACK=ON"
+          "-DUSE_SYSTEM_NANOFLANN=ON"
+          "-DUSE_SYSTEM_OPENSSL=OFF" # problems with curl
+          "-DUSE_SYSTEM_PNG=ON"
+          "-DUSE_SYSTEM_OPENGL=ON"
+          #"-DUSE_SYSTEM_PYBIND11=ON"
+          "-DUSE_SYSTEM_QHULLCPP=ON"
+          "-DUSE_SYSTEM_STDGPU=ON"
+          "-DUSE_SYSTEM_TBB=ON"
+          "-DUSE_SYSTEM_TINYGLTF=OFF"
+          "-DUSE_SYSTEM_TINYOBJLOADER=ON"
+          "-DUSE_SYSTEM_VTK=ON"
+          "-DUSE_SYSTEM_ZEROMQ=ON"
+          "-DUVATLAS_LIBRARIES=/tmp/dummy"
+        ];
+      };
+
+      mesh-voxelization = pkgs.stdenv.mkDerivation rec {
+        pname = "mesh-voxelization";
+        version = "2022-02-10";
+
+        src = pkgs.fetchFromGitHub {
+          owner = "davidstutz";
+          repo = pname;
+          rev = "81a237c3b345062e364b180a8a4fc7ac98e107a4";
+          hash = "sha256-HSSV3KajrTnX7oWtC+JYXuuXu8pkcVz63wFm+ADx2rk=";
+        };
+        nativeBuildInputs = with pkgs; [cmake ninja];
+        buildInputs = with pkgs; [
+          hdf5
+          boost
+          eigen
+        ];
+      };
+
       vtk = pkgs.vtk.overrideAttrs rec {
         postFixup = ''
           # hack together includes
           cp -rv $out/include/vtk/* $out/include/
         '';
       };
+
+      vtkbool = pkgs.stdenv.mkDerivation rec {
+        pname = "vtkbool";
+        version = "3.0.1";
+
+        src = pkgs.fetchFromGitHub {
+          owner = "zippy84";
+          repo = pname;
+          rev = "v${version}";
+          hash = "sha256-3yMu8GiqRSsgjyI+oKmO7ekvQVSRJlf8G/FxR9JaPHM=";
+        };
+
+        nativeBuildInputs = with pkgs; [cmake ninja];
+        buildInputs = with pkgs; [vtk];
+
+        cmakeFlags = [
+          "-DCMAKE_INSTALL_LIBDIR=lib"
+          "-DCMAKE_INSTALL_INCLUDEDIR=include"
+          "-DCMAKE_INSTALL_BINDIR=bin"
+        ];
+
+        postFixup = ''
+          cp ${src}/Utilities.h $out/include
+        '';
+      };
+
       # use our own cgal package instead of upstream to enable qt support
       cgal-qt = pkgs.stdenv.mkDerivation rec {
         pname = "cgal";
@@ -58,6 +190,8 @@
         #pkgs.mpfr
         #pkgs.boost
         packages.x86_64-linux.vtk
+        #packages.x86_64-linux.vtkbool
+        pkgs.cgal
 
         # this is for the shellhook portion
         #qt6.wrapQtAppsHook
--- main.cpp
+++ main.cpp
@@ -15,66 +15,450 @@
 #include <vtkDataSetMapper.h>
 #include <vtkFeatureEdges.h>
 #include <vtkStripper.h>
+#include <vtkFillHolesFilter.h>
 #include <vtkCallbackCommand.h>
+#include <vtkPolyDataNormals.h>
+#include <vtkImageData.h>
+#include <vtkPolyDataToImageStencil.h>
+#include <vtkImageStencil.h>
+#include <vtkImageStencilAlgorithm.h>
+#include <vtkImageStencilSource.h>
+#include <vtkPointData.h>
+#include <vtkCleanPolyData.h>
+#include <vtkAlgorithm.h>
+#include <vtkCubeSource.h>
+#include <vtkImageActor.h>
+#include <vtkCollisionDetectionFilter.h>
+#include <vtkImageStencilToImage.h>
+#include <vtkTriangleFilter.h>
+#include <vtkImageStencil.h>
+#include <vtkMatrix4x4.h>
+#include <vtkTransform.h>
+#include <vtkAxesActor.h>
+#include <vtkDataSetMapper.h>
+#include <vtkOrientationMarkerWidget.h>
+#include <vtkImageProperty.h>
+#include <vtkSmartVolumeMapper.h>
+#include <vtkImageViewer2.h>
+#include <vtkVolumeProperty.h>
+#include <vtkImageReslice.h>
 #include <array>
 
-void CameraModifiedCallback(vtkObject* caller,
-                            long unsigned int vtkNotUsed(eventId),
-                            void* vtkNotUsed(clientData),
-                            void* vtkNotUsed(callData))
-{
-  std::cout << caller->GetClassName() << " modified" << std::endl;
-  vtkCamera* camera = static_cast<vtkCamera*>(caller);
-  // Print the interesting stuff.
-  std::cout << "  auto camera = renderer->GetActiveCamera();" << std::endl;
-  std::cout << "  camera->SetPosition(" << camera->GetPosition()[0] << ", "
-            << camera->GetPosition()[1] << ", " << camera->GetPosition()[2]
-            << ");" << std::endl;
-  std::cout << "  camera->SetFocalPoint(" << camera->GetFocalPoint()[0] << ", "
-            << camera->GetFocalPoint()[1] << ", " << camera->GetFocalPoint()[2]
-            << ");" << std::endl;
-  std::cout << "  camera->SetViewUp(" << camera->GetViewUp()[0] << ", "
-            << camera->GetViewUp()[1] << ", " << camera->GetViewUp()[2] << ");"
-            << std::endl;
-  std::cout << "  camera->SetDistance(" << camera->GetDistance() << ");"
-            << std::endl;
-  std::cout << "  camera->SetClippingRange(" << camera->GetClippingRange()[0]
-            << ", " << camera->GetClippingRange()[1] << ");" << std::endl;
-  std::cout << std::endl;
-}
+
+struct Printer {
+    float Length; // mm
+    float Width;
+    float Height;
+    int ResolutionX;
+    int ResolutionY;
+    int ResolutionZ;
+    float BlockX(){return Length/ResolutionX;};
+    float BlockY(){return Width/ResolutionY;};
+    float BlockZ(){return Height/ResolutionZ;};
+};
 
 int main(int, char*[])
 {
+    Printer Saturn2 = Printer{
+            .Length= 218.88,
+            .Width=123.12,
+            .Height=250,
+            .ResolutionX=7680,
+            .ResolutionY=4320,
+            .ResolutionZ=10000,
+    };
+
+
+    vtkNew<vtkImageData> imageData;
+    imageData->SetDimensions(1000,1000,1000);
+    imageData->SetSpacing(1.0, 1.0, 1.0);
+    imageData->SetOrigin(0.0, 0.0, 0.0);
+
+
+
+
+    //vtkNew<vtkPoints> points;
+
+  //vtkSmartPointer<vtkSTLReader> stlReader {vtkSmartPointer<vtkSTLReader>::New()};
   vtkNew<vtkSTLReader> stlReader;
   stlReader->SetFileName("/mnt/chump/3dDruck/sechsschwaenzer/Dispenser_with_Funnel.stl");
   stlReader->Update();
+
+
+  auto polyData = stlReader->GetOutput();
+double bounds[6];
+polyData->GetBounds(bounds);
+
+
+  if (polyData->GetNumberOfPoints() < 2 || polyData->GetNumberOfCells() < 2)
+  {
+    std::cout << "Convert: Cannot create binary labelmap from surface with number of points: "
+        << polyData->GetNumberOfPoints() << " and number of cells: "
+        << polyData->GetNumberOfCells() << std::endl;
+  }
+
+  // Compute polydata normals
+  vtkNew<vtkPolyDataNormals> normalFilter;
+  normalFilter->SetInputData(polyData);
+  normalFilter->ConsistencyOn();
+
+  // Make sure that we have a clean triangle polydata
+  vtkNew<vtkTriangleFilter> triangle;
+  triangle->SetInputConnection(normalFilter->GetOutputPort());
+
+  // Convert to triangle strip
+  vtkNew<vtkStripper> stripper;
+  stripper->SetInputConnection(triangle->GetOutputPort());
+
+  // Convert polydata to stencil
+  vtkNew<vtkPolyDataToImageStencil> polyDataToImageStencil;
+  polyDataToImageStencil->SetInputConnection(stripper->GetOutputPort());
+  polyDataToImageStencil->SetOutputSpacing(imageData->GetSpacing());
+  polyDataToImageStencil->SetOutputOrigin(bounds[0], bounds[2], bounds[4]);
+  polyDataToImageStencil->SetOutputWholeExtent(imageData->GetExtent());
+  polyDataToImageStencil->Update();
+
+  // Convert stencil to image
+  vtkNew<vtkImageStencilToImage> imageStencilToImage;
+  imageStencilToImage->SetInputConnection(polyDataToImageStencil->GetOutputPort());
+  imageStencilToImage->SetOutsideValue(0);
+  imageStencilToImage->SetInsideValue(255);
+  imageStencilToImage->SetOutput(imageData);
+  imageStencilToImage->Update();
+
+
+    vtkNew<vtkTransform> transform;
+    transform->Translate(0,0,60);
+
+  vtkNew<vtkImageReslice> reslice;
+  reslice->SetOutputExtent(bounds[0],300,bounds[2],300,0,1);
+  reslice->SetInputData(imageData);
+  reslice->SetOutputSpacing(1,1,1);
+  reslice->SetOutputOrigin(0,0,50);
+  //reslice->SetResliceTransform(transform);
+  reslice->Update();
+
+  vtkNew<vtkImageViewer2> imageViewer;
+    imageViewer->SetInputConnection(reslice->GetOutputPort());
+    vtkNew<vtkRenderWindowInteractor> renderWindowInteractor;
+    imageViewer->SetupInteractor(renderWindowInteractor);;
+    imageViewer->GetRenderWindow()->SetWindowName("ImageRotate");
+    imageViewer->GetRenderer()->ResetCamera();
+    imageViewer->Render();
+
+
+    /*
+
+    int* dims = imageData->GetDimensions();
+    for (int z=0; z<dims[2]; z++)
+    {
+        reslice->SetOutputOrigin(0,0,z);
+        reslice->Update();
+        imageViewer->Render();
+        std::cout << z << std::endl;
+    }
+    */
+
+    renderWindowInteractor->Start();
+
+
+
+    return EXIT_SUCCESS;
+
+
+  /*
+  vtkNew<vtkSmartVolumeMapper> volmapper;
+  volmapper->SetInputData(imageData);
+
+  vtkNew<vtkVolumeProperty> volProp;
+
+
+  vtkNew<vtkVolume> volume;
+  volume->SetMapper(volmapper);
+  volume->SetProperty(volProp);
+
+
+
+
+
+
+
+  vtkNew<vtkRenderer> renderer;
+  renderer->AddVolume(volume);
+  renderer->ResetCamera();
+
+  vtkNew<vtkRenderWindow> renderWindow;
+  renderWindow->SetSize(1920, 1080);
+  //renderWindow->SetOffScreenRendering(true);
+  renderWindow->AddRenderer(renderer);
+  renderWindow->SetWindowName("Slicer Preview");
+
+
+  vtkNew<vtkRenderWindowInteractor> interactor;
+  interactor->SetRenderWindow(renderWindow);
+
+    interactor->Start();
+  /*
+
+
+
+
+
+
+
+
+  //vtkSmartPointer<vtkImageData> id = PolyDataToImageData(cleanFilter->GetOutput(), 500);
+
+  vtkNew<vtkDataSetMapper> imageMapper;
+  imageMapper->SetInputData(id);
+
+  vtkNew<vtkActor> imageActor;
+  imageActor->SetMapper(imageMapper);
+  imageActor->GetProperty()->SetColor(1,1,1);
+
+
+
+  vtkNew<vtkRenderer> renderer;
+  renderer->AddActor(imageActor);
+  renderer->ResetCamera();
+  //renderer->GetActiveCamera()->SetParallelProjection(true);
+  //renderer->GetActiveCamera()->SetFocalPoint(0,0,0);
+
+  vtkNew<vtkRenderWindow> renderWindow;
+  renderWindow->SetSize(1920, 1080);
+  //renderWindow->SetOffScreenRendering(true);
+  renderWindow->AddRenderer(renderer);
+  renderWindow->SetWindowName("Slicer Preview");
+
+  vtkNew<vtkRenderWindowInteractor> interactor;
+  interactor->SetRenderWindow(renderWindow);
+
+
+  vtkNew<vtkAxesActor> axes;
+  vtkNew<vtkOrientationMarkerWidget> mw;
+
+  mw->SetOrientationMarker(axes);
+  mw->SetInteractor(interactor);
+  mw->SetViewport(0.0,0.0,0.4,0.4);
+  mw->SetEnabled(1);
+  mw->InteractiveOn();
+
+  interactor->Start();
+
+  /*
+
+  vtkNew<vtkCubeSource> cube;
+  cube->SetXLength(Saturn2.BlockX());
+  cube->SetYLength(Saturn2.BlockY());
+  cube->SetZLength(Saturn2.BlockZ());
+
+
+  vtkNew<vtkMatrix4x4> matrix1;
+  vtkNew<vtkTransform> transform0;
+
+
+  vtkNew<vtkCollisionDetectionFilter> collide;
+  collide->SetInputConnection(0,stlReader->GetOutputPort());
+  collide->SetTransform(0, transform0);
+  collide->SetInputConnection(1,cube->GetOutputPort());
+  collide->SetMatrix(1, matrix1);
+  collide->SetBoxTolerance(0.0);
+  collide->SetCellTolerance(0.0);
+  collide->SetCollisionModeToAllContacts();
+  collide->GenerateScalarsOn();
+
+  for (int z=0; z<Saturn2.ResolutionZ; z++) {
+      for (int y=0; y<Saturn2.ResolutionY; y++) {
+          for (int x=0; x<Saturn2.ResolutionX; x++) {
+              cube->SetCenter(x*Saturn2.BlockX(), y*Saturn2.BlockY(), z*Saturn2.BlockZ());
+              cube->Update();
+              collide->Update();
+              if (collide->GetNumberOfContacts()>=1) {
+                  points->InsertNextPoint(x,y,z);
+              }
+
+
+          }
+      }
+  }
+
+  return EXIT_SUCCESS;
+
+
+  // stl 1 == 1 mm
+
+  /*
+
+
+  vtkNew<vtkPlane> planea;
+  planea->SetOrigin(0,0,10);
+  planea->SetNormal(0, 0, -1);
+
+  vtkNew<vtkCubeSource> cube;
+  cube->SetYLength(200);
+  cube->SetXLength(200);
+  cube->SetZLength(200);
+  cube->SetCenter(0,0,150);
+
+  vtkNew<vtkCylinderSource> cyl;
+  cyl->SetResolution(32);
+  cyl->SetHeight(1);
+  cyl->SetCenter(0,.5,0);
+
+  vtkNew<vtkPolyDataBooleanFilter> slice;
+  slice->SetInputConnection(0, stlReader->GetOutputPort());
+  slice->SetInputConnection(1, cube->GetOutputPort());
+  slice->SetOperModeToDifference();
+
+  //slice->S
+  slice->Update();
+
+  vtkNew<vtkDataSetMapper> mapper;
+  mapper->SetInputConnection(slice->GetOutputPort(0));
+
+
+  vtkNew<vtkDataSetMapper> cmapper;
+  cmapper->SetInputConnection(cube->GetOutputPort());
+  vtkNew<vtkActor> cactor;
+  cactor->GetProperty()->SetOpacity(.2);
+  cactor->SetMapper(cmapper);
+
+  vtkNew<vtkDataSetMapper> pmapper;
+  pmapper->SetInputConnection(stlReader->GetOutputPort());
+  vtkNew<vtkActor> pactor;
+  pactor->GetProperty()->SetOpacity(.2);
+  pactor->SetMapper(pmapper);
+
+  vtkNew<vtkActor> actor;
+  actor->SetMapper(mapper);
+  //actor->GetProperty()->SetColor(1,1,1);
+  //actor->GetProperty()->SetAmbient(1);
+  //actor->GetProperty()->SetDiffuse(0);
+  //actor->GetProperty()->SetSpecular(0);
+
+
+  vtkNew<vtkRenderer> renderer;
+  renderer->AddActor(actor);
+  //renderer->AddActor(cactor);
+  //renderer->AddActor(pactor);
+  renderer->ResetCamera();
+  renderer->GetActiveCamera()->SetParallelProjection(true);
+  renderer->GetActiveCamera()->SetFocalPoint(0,0,0);
+
+  vtkNew<vtkRenderWindow> renderWindow;
+  renderWindow->SetSize(1920, 1080);
+  //renderWindow->SetOffScreenRendering(true);
+  renderWindow->AddRenderer(renderer);
+  renderWindow->SetWindowName("Slicer Preview");
+
+  vtkNew<vtkRenderWindowInteractor> interactor;
+  interactor->SetRenderWindow(renderWindow);
+  interactor->Start();
+
+  /*
+
+  double bounds[6];
+   stlReader->GetOutput()->GetBounds(bounds);
+   double spacing[3]; // desired volume spacing
+   spacing[0] = 0.05;
+   spacing[1] = 0.05;
+   spacing[2] = 0.05;
+
+   int dim[3];
+     for (int i = 0; i < 3; i++)
+     {
+       dim[i] = static_cast<int>(
+           ceil((bounds[i * 2 + 1] - bounds[i * 2]) / spacing[i]));
+     }
+
+     double origin[3];
+     origin[0] = bounds[0] + spacing[0] / 2;
+     origin[1] = bounds[2] + spacing[1] / 2;
+     origin[2] = bounds[4] + spacing[2] / 2;
+
+
+  vtkNew<vtkImageData> whiteImage;
+  whiteImage->SetSpacing(spacing);
+  whiteImage->SetDimensions(dim);
+  whiteImage->SetExtent(0,dim[0]-1,0,dim[1]-1,0,dim[2]-1);
+  whiteImage->SetOrigin(origin);
+  whiteImage->AllocateScalars(VTK_UNSIGNED_CHAR, 1);
+
+  vtkIdType count = whiteImage->GetNumberOfPoints();
+  for (vtkIdType i = 0; i < count; ++i)
+  {
+    whiteImage->GetPointData()->GetScalars()->SetTuple1(i, 255);
+  }
+
+  vtkNew<vtkPolyDataToImageStencil> p2i;
+  //p2i->SetEnableSMP(true);
+  p2i->SetInputData(stlReader->GetOutput());
+  p2i->SetOutputOrigin(whiteImage->GetOrigin());
+  p2i->SetOutputSpacing(whiteImage->GetSpacing());
+  p2i->SetOutputWholeExtent(whiteImage->GetExtent());
+  p2i->Update();
+
+
+  vtkNew<vtkImageStencil> imgstenc;
+  imgstenc->SetInputData(whiteImage);
+  imgstenc->SetStencilConnection(p2i->GetOutputPort());
+  //imgstenc->ReverseStencilOff();
+  imgstenc->SetBackgroundValue(0);
+  imgstenc->Update();
+
+  vtkNew<vtkDataSetMapper> imageMapper;
+  imageMapper->SetInputConnection(imgstenc->GetOutputPort());
+
+  vtkNew<vtkActor> imageActor;
+  imageActor->SetMapper(imageMapper);
+  imageActor->GetProperty()->SetColor(1,1,1);
+  imageActor->GetProperty()->SetAmbient(1);
+  imageActor->GetProperty()->SetDiffuse(0);
+  imageActor->GetProperty()->SetSpecular(0);
+
+
+  vtkNew<vtkRenderer> renderer;
+  renderer->AddActor(imageActor);
+  renderer->ResetCamera();
+  renderer->GetActiveCamera()->SetParallelProjection(true);
+  renderer->GetActiveCamera()->SetFocalPoint(0,0,0);
+
+  vtkNew<vtkRenderWindow> renderWindow;
+  renderWindow->SetSize(1920, 1080);
+  //renderWindow->SetOffScreenRendering(true);
+  renderWindow->AddRenderer(renderer);
+  renderWindow->SetWindowName("Slicer Preview");
+
+  vtkNew<vtkRenderWindowInteractor> interactor;
+  interactor->SetRenderWindow(renderWindow);
+  interactor->Start();
+
+  /*
+
+
   vtkNew<vtkPolyDataMapper> stlMapper;
   stlMapper->SetInputConnection(stlReader->GetOutputPort());
 
+  vtkNew<vtkPlane> planea;
+  planea->SetOrigin(0,0,10);
+  planea->SetNormal(0, 0, -1);
 
-  // The actor is a grouping mechanism: besides the geometry (mapper), it
-  // also has a property, transformation matrix, and/or texture map.
-  // Here we set its color and rotate it around the X and Y axes.
-  vtkNew<vtkActor> stlActor;
-  stlActor->SetMapper(stlMapper);
-  //stlActor->GetProperty()->SetColor(1,1,1);
-  //stlActor->GetProperty()->SetAmbient(1);
-  //stlActor->GetProperty()->SetDiffuse(0);
-  //stlActor->GetProperty()->SetSpecular(0);
-  stlActor->SetPosition(0,0,0);
-  //stlActor->RotateX(30.0);
-  //stlActor->RotateY(-45.0);
+  vtkNew<vtkPlane> planeb;
+  planeb->SetOrigin(0,0,9.5);
+  planeb->SetNormal(0, 0, 1);
 
-
-  vtkNew<vtkPlane> plane;
-  plane->SetOrigin(0,0,10);
-  plane->SetNormal(0, 0, -1);
+  vtkNew<vtkClipPolyData> clippera;
+  clippera->SetInputData(stlReader->GetOutput());
+  clippera->SetClipFunction(planea);
+  //clipper->GenerateClippedOutputOn();
+  clippera->Update();
 
   vtkNew<vtkClipPolyData> clipper;
-  clipper->SetInputData(stlReader->GetOutput());
-  clipper->SetClipFunction(plane);
-  clipper->SetValue(0);
+  clipper->SetInputData(clippera->GetOutput());
+  clipper->SetClipFunction(planeb);
+  //clipper->GenerateClippedOutputOn();
   clipper->Update();
+
 
   vtkNew<vtkDataSetMapper> clipMapper;
   clipMapper->SetInputData(clipper->GetOutput());
@@ -82,44 +466,25 @@ int main(int, char*[])
   vtkNew<vtkActor> clipActor;
   clipActor->SetMapper(clipMapper);
   clipActor->GetProperty()->SetColor(1,1,1);
-  //clipActor->GetProperty()->SetAmbient(1);
-  //clipActor->GetProperty()->SetDiffuse(0);
-  //clipActor->GetProperty()->SetSpecular(0);
-  clipActor->SetPosition(0,0,0);
+  clipActor->GetProperty()->SetAmbient(1);
+  clipActor->GetProperty()->SetDiffuse(0);
+  clipActor->GetProperty()->SetSpecular(0);
+  //clipActor->SetPosition(0,0,0);
 
-  // Now extract feature edges
-  vtkNew<vtkFeatureEdges> boundaryEdges;
-  boundaryEdges->SetInputData(clipper->GetOutput());
-  boundaryEdges->BoundaryEdgesOn();
-  boundaryEdges->FeatureEdgesOff();
-  boundaryEdges->NonManifoldEdgesOff();
-  boundaryEdges->ManifoldEdgesOff();
-
-  vtkNew<vtkStripper> boundaryStrips;
-  boundaryStrips->SetInputConnection(boundaryEdges->GetOutputPort());
-  boundaryStrips->Update();
-
-  // Change the polylines into polygons
-  vtkNew<vtkPolyData> boundaryPoly;
-  boundaryPoly->SetPoints(boundaryStrips->GetOutput()->GetPoints());
-  boundaryPoly->SetPolys(boundaryStrips->GetOutput()->GetLines());
-
-  vtkNew<vtkPolyDataMapper> boundaryMapper;
-  boundaryMapper->SetInputData(boundaryPoly);
-
-  vtkNew<vtkActor> boundaryActor;
-  boundaryActor->SetMapper(boundaryMapper);
-  boundaryActor->GetProperty()->SetColor(1,0,0);
-  //boundaryActor->GetProperty()->SetAmbient(1);
-  //boundaryActor->GetProperty()->SetDiffuse(0);
-  //boundaryActor->GetProperty()->SetSpecular(0);
+  vtkNew<vtkProperty> backFaces;
+  backFaces->SetSpecular(0);
+  backFaces->SetDiffuse(0);
+  backFaces->SetAmbient(1);
+  backFaces->SetAmbientColor(1,1,1);
+  clipActor->SetBackfaceProperty(backFaces);
 
   // The renderer generates the image
   // which is then displayed on the render window.
   // It can be thought of as a scene to which the actor is added
   vtkNew<vtkRenderer> renderer;
   renderer->AddActor(clipActor);
-  renderer->AddActor(boundaryActor);
+  //renderer->AddActor(clippedAwayActor);
+  //renderer->AddActor(boundaryActor);
   renderer->ResetCamera();
   renderer->GetActiveCamera()->SetParallelProjection(true);
   renderer->GetActiveCamera()->SetFocalPoint(0,0,0);
@@ -138,19 +503,20 @@ int main(int, char*[])
   renderWindow->SetWindowName("Slicer Preview");
 
 
-  /*
+/*
   for (double i=250; i<=1000; i+=0.1) {
       plane->SetOrigin(0,0,i);
       clipper->Update();
-      boundaryStrips->Update();
-      renderer->GetActiveCamera()->SetClippingRange(i,i+0.1);
+      //boundaryStrips->Update();
+      //renderer->GetActiveCamera()->SetClippingRange(i,i+0.1);
       renderWindow->Render();
   }
   */
 
-  vtkNew<vtkRenderWindowInteractor> interactor;
-  interactor->SetRenderWindow(renderWindow);
-  interactor->Start();
+
+  //vtkNew<vtkRenderWindowInteractor> interactor;
+  //interactor->SetRenderWindow(renderWindow);
+  //interactor->Start();
 
 
 
